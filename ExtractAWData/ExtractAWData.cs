3a5fccc8f732e949fbdf7d56c44a7def5629d571
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-08-09 15:28:36 +0100

    feat: specifically throw errors

--- ExtractAWData/ExtractAWData.cs
+++ ExtractAWData/ExtractAWData.cs
@@ -55,104 +55,112 @@ namespace Defra.Gwa.Etl
         [Function("ExtractAWData")]
         public async Task Run([TimerTrigger("0 0 8 * * 0")] MyInfo myTimer)
         {
-            logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-
-            int pageSize = 500; // default is 500 prefer to be specific
-            int page = 0; // zero based
-            bool morePages;
-            int deviceCount = 0;
-            int iPadCount = 0;
-            int noEmailCount = 0;
-            int noPhoneNumberCount = 0;
-
-            Dictionary<string, User> users = new();
-            do
+            try
             {
-                baseUri.Query = $"page={page}&pagesize={pageSize}";
-                page++;
-                Uri uri = baseUri.Uri;
+                logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
-                HttpRequestMessage req = new(HttpMethod.Get, uri);
-                req.Headers.Add("Authorization", authorizationHeader);
-                req.Headers.Add("aw-tenant-code", awTenantCode);
-                req.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                int pageSize = 500; // default is 500 prefer to be specific
+                int page = 0; // zero based
+                bool morePages;
+                int deviceCount = 0;
+                int iPadCount = 0;
+                int noEmailCount = 0;
+                int noPhoneNumberCount = 0;
 
-                HttpClient httpClient = httpClientFactory.CreateClient();
-                HttpResponseMessage res = await httpClient.SendAsync(req);
-                logger.LogInformation($"Request - {res.RequestMessage}");
-                logger.LogInformation($"Response - {res}");
-                _ = res.EnsureSuccessStatusCode();
-
-                AirWatchApiResponse responseData = await res.Content.ReadFromJsonAsync<AirWatchApiResponse>();
-
-                IList<Device> Devices = responseData.Devices;
-                int Page = responseData.Page;
-                int PageSize = responseData.PageSize;
-                int Total = responseData.Total;
-                int resDeviceCount = Devices.Count;
-                logger.LogInformation($"DeviceCount: {resDeviceCount}");
-                logger.LogInformation($"Page: {Page}\nPageSize: {PageSize}\nTotal: {Total}");
-
-                for (int i = 0; i < resDeviceCount; i++)
+                Dictionary<string, User> users = new();
+                do
                 {
-                    deviceCount++;
-                    Device device = Devices[i];
-                    ModelId ModelId = device.ModelId;
-                    string phoneNumber = device.PhoneNumber;
-                    string emailAddress = device.UserEmailAddress.ToLower(new CultureInfo("en-GB"));
+                    baseUri.Query = $"page={page}&pagesize={pageSize}";
+                    page++;
+                    Uri uri = baseUri.Uri;
 
-                    if (ModelId.Id.Value == 2)
+                    HttpRequestMessage req = new(HttpMethod.Get, uri);
+                    req.Headers.Add("Authorization", authorizationHeader);
+                    req.Headers.Add("aw-tenant-code", awTenantCode);
+                    req.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpClient httpClient = httpClientFactory.CreateClient();
+                    HttpResponseMessage res = await httpClient.SendAsync(req);
+                    logger.LogInformation($"Request - {res.RequestMessage}");
+                    logger.LogInformation($"Response - {res}");
+                    _ = res.EnsureSuccessStatusCode();
+
+                    AirWatchApiResponse responseData = await res.Content.ReadFromJsonAsync<AirWatchApiResponse>();
+
+                    IList<Device> Devices = responseData.Devices;
+                    int Page = responseData.Page;
+                    int PageSize = responseData.PageSize;
+                    int Total = responseData.Total;
+                    int resDeviceCount = Devices.Count;
+                    logger.LogInformation($"DeviceCount: {resDeviceCount}");
+                    logger.LogInformation($"Page: {Page}\nPageSize: {PageSize}\nTotal: {Total}");
+
+                    for (int i = 0; i < resDeviceCount; i++)
                     {
-                        iPadCount++;
-                    }
-                    else
-                    {
-                        if (!string.IsNullOrWhiteSpace(emailAddress))
+                        deviceCount++;
+                        Device device = Devices[i];
+                        ModelId ModelId = device.ModelId;
+                        string phoneNumber = device.PhoneNumber;
+                        string emailAddress = device.UserEmailAddress.ToLower(new CultureInfo("en-GB"));
+
+                        if (ModelId.Id.Value == 2)
                         {
-                            _ = users.TryGetValue(emailAddress, out User user);
-                            if (!string.IsNullOrWhiteSpace(phoneNumber))
+                            iPadCount++;
+                        }
+                        else
+                        {
+                            if (!string.IsNullOrWhiteSpace(emailAddress))
                             {
-                                if (user != null)
+                                _ = users.TryGetValue(emailAddress, out User user);
+                                if (!string.IsNullOrWhiteSpace(phoneNumber))
                                 {
-                                    if (!user.PhoneNumbers.Contains(phoneNumber))
+                                    if (user != null)
                                     {
-                                        user.PhoneNumbers.Add(phoneNumber);
+                                        if (!user.PhoneNumbers.Contains(phoneNumber))
+                                        {
+                                            user.PhoneNumbers.Add(phoneNumber);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        user = new User { EmailAddress = emailAddress, PhoneNumbers = new List<string> { phoneNumber } };
+                                        users.Add(emailAddress, user);
                                     }
                                 }
                                 else
                                 {
-                                    user = new User { EmailAddress = emailAddress, PhoneNumbers = new List<string> { phoneNumber } };
-                                    users.Add(emailAddress, user);
+                                    if (user == null)
+                                    {
+                                        users.Add(emailAddress, new User { EmailAddress = emailAddress, PhoneNumbers = new List<string>() });
+                                    }
+                                    noPhoneNumberCount++;
                                 }
                             }
                             else
                             {
-                                if (user == null)
-                                {
-                                    users.Add(emailAddress, new User { EmailAddress = emailAddress, PhoneNumbers = new List<string>() });
-                                }
-                                noPhoneNumberCount++;
+                                noEmailCount++;
                             }
                         }
-                        else
-                        {
-                            noEmailCount++;
-                        }
                     }
+                    logger.LogInformation($"Processed {deviceCount} devices.");
+                    morePages = page * pageSize < Total;
+                } while (morePages);
+
+                using (MemoryStream json = new(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(users.Values))))
+                {
+                    _ = await blobClient.UploadAsync(json, true);
                 }
-                logger.LogInformation($"Processed {deviceCount} devices.");
-                morePages = page * pageSize < Total;
-            } while (morePages);
 
-            using (MemoryStream json = new(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(users.Values))))
-            {
-                _ = await blobClient.UploadAsync(json, true);
+                logger.LogInformation($"Data extract from AW is complete.\n{deviceCount} devices have been processed.");
+                logger.LogInformation($"{users.Count} devices have a UserEmailAddress of which {noPhoneNumberCount} have no PhoneNumber.");
+                logger.LogInformation($"{noEmailCount} devices with no UserEmailAddress.");
+                logger.LogInformation($"{iPadCount} iPads have been ignored.");
             }
-
-            logger.LogInformation($"Data extract from AW is complete.\n{deviceCount} devices have been processed.");
-            logger.LogInformation($"{users.Count} devices have a UserEmailAddress of which {noPhoneNumberCount} have no PhoneNumber.");
-            logger.LogInformation($"{noEmailCount} devices with no UserEmailAddress.");
-            logger.LogInformation($"{iPadCount} iPads have been ignored.");
+            catch (HttpRequestException ex)
+            {
+                logger.LogError(ex.ToString());
+                throw;
+            }
         }
     }
 }
--- Tests/ExtractAWDataTest.cs
+++ Tests/ExtractAWDataTest.cs
@@ -36,29 +36,83 @@ namespace ExtractAWDataTests
             configuration = new ConfigurationBuilder().AddInMemoryCollection(config).Build();
         }
 
-        [Fact]
-        public async void Test1()
+        private static IHttpClientFactory SetupHttpClientFactory(Mock<IHttpClientFactory> httpClientFactoryMock, HttpResponseMessage responseMessage)
         {
-            AirWatchApiResponse apiResponse = new() { Devices = new List<Device>() };
-            JObject json = (JObject)JToken.FromObject(apiResponse);
             Mock<HttpMessageHandler> handlerMock = new(MockBehavior.Strict);
             handlerMock
                 .Protected()
                 .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
-                .ReturnsAsync(new HttpResponseMessage() { StatusCode = HttpStatusCode.OK, Content = new StringContent(json.ToString()) })
+                .ReturnsAsync(responseMessage)
                 .Verifiable();
             HttpClient httpClient = new(handlerMock.Object);
             _ = httpClientFactoryMock.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(httpClient);
-            ExtractAWData extractAWData = new(blobClientMock.Object, configuration, httpClientFactoryMock.Object, loggerMock.Object);
+            return httpClientFactoryMock.Object;
+        }
+
+        private static IHttpClientFactory SetupHttpClientFactoryForFailure(Mock<IHttpClientFactory> httpClientFactoryMock)
+        {
+            HttpResponseMessage responseMessage = new() { StatusCode = HttpStatusCode.BadRequest };
+            return SetupHttpClientFactory(httpClientFactoryMock, responseMessage);
+        }
+
+        private static IHttpClientFactory SetupHttpClientFactoryForSuccess(Mock<IHttpClientFactory> httpClientFactoryMock, AirWatchApiResponse apiResponse)
+        {
+            JObject json = (JObject)JToken.FromObject(apiResponse);
+            HttpResponseMessage responseMessage = new() { StatusCode = HttpStatusCode.OK, Content = new StringContent(json.ToString()) };
+            return SetupHttpClientFactory(httpClientFactoryMock, responseMessage);
+        }
+
+        private static void VerifyLogError(Mock<ILogger<ExtractAWData>> loggerMock, string message)
+        {
+            loggerMock.Verify(logger => logger.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((o, t) => o.ToString().Contains(message)),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()
+            ));
+        }
+
+        private static void VerifyLogInformation(Mock<ILogger<ExtractAWData>> loggerMock, string message)
+        {
+            loggerMock.Verify(logger => logger.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((o, t) => o.ToString().Contains(message)),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()
+            ));
+        }
+
+        [Fact]
+        public async void UnsuccessfulStatusCodeThrowsAndLogsError()
+        {
+            IHttpClientFactory httpClientFactory = SetupHttpClientFactoryForFailure(httpClientFactoryMock);
+
+            ExtractAWData extractAWData = new(blobClientMock.Object, configuration, httpClientFactory, loggerMock.Object);
+            _ = await Assert.ThrowsAsync<HttpRequestException>(() => extractAWData.Run(null));
+
+            VerifyLogError(loggerMock, "Response status code does not indicate success: 400 (Bad Request).");
+        }
+
+        [Fact]
+        public async void NoDevicesReturnedLogsInformation()
+        {
+            AirWatchApiResponse apiResponse = new() { Devices = new List<Device>() };
+            IHttpClientFactory httpClientFactory = SetupHttpClientFactoryForSuccess(httpClientFactoryMock, apiResponse);
+
+            ExtractAWData extractAWData = new(blobClientMock.Object, configuration, httpClientFactory, loggerMock.Object);
             await extractAWData.Run(null);
 
-            loggerMock.Verify(logger => logger.Log(
-                        LogLevel.Information,
-                        It.IsAny<EventId>(),
-                        It.Is<It.IsAnyType>((o, t) => o.ToString().StartsWith("C# Timer trigger function executed at: ", StringComparison.InvariantCulture)),
-                        It.IsAny<Exception>(),
-                        It.IsAny<Func<It.IsAnyType, Exception, string>>()
-                        ));
+            VerifyLogInformation(loggerMock, "C# Timer trigger function executed at:");
+            VerifyLogInformation(loggerMock, "Request - ");
+            VerifyLogInformation(loggerMock, "Response - StatusCode: 200, ReasonPhrase: 'OK', Version: 1.1, Content: System.Net.Http.StringContent");
+            VerifyLogInformation(loggerMock, "DeviceCount: 0");
+            VerifyLogInformation(loggerMock, "Page: 0\nPageSize: 0\nTotal: 0");
+            VerifyLogInformation(loggerMock, "Data extract from AW is complete.\n0 devices have been processed.");
+            VerifyLogInformation(loggerMock, "0 devices have a UserEmailAddress of which 0 have no PhoneNumber.");
+            VerifyLogInformation(loggerMock, "0 devices with no UserEmailAddress.");
+            VerifyLogInformation(loggerMock, "0 iPads have been ignored.");
         }
     }
 }
